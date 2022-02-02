acfcfaf57d29826c5c649b6140ac0549f1f4711e
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2022-02-02 23:11:44 +0100

    Add marker toggle back in

--- src/Map/MainLayers/Layer-makers.js
+++ src/Map/MainLayers/Layer-makers.js
@@ -9,30 +9,35 @@ export function MarkerLayer( { markers_visible, highlight_id } ) {
     return (
         <>
             <Source id="marker-source" type="geojson" data={markers_geojson} promoteId={"#"}>
-                <Layer
-                    id="flag-layer"
-                    type="symbol"
-                    layout={{ "icon-image": ["get", "project_type"], "icon-size": 0.6, "icon-anchor": "bottom-left",   "icon-allow-overlap": true, }}
-                    source="marker-source"
-                    filter={["==", ["get", "#"], highlight_id_or_str]}
-                />
-                <Layer
-                    id="markers-layer"
-                    type="circle"
-                    // type="symbol"
-                    // layout={{ "icon-image": "custom-marker", "icon-size": 0.05 }}
-                    source="marker-source"
-                    paint = {{ "circle-stroke-width": 0.5,
-                        "circle-color": [
-                            "match",
-                            ["get", "#"], highlight_id_or_str,
-                            "red",
-                            "transparent"
-                        ],
-                        "circle-stroke-color": "red",
-                        "circle-radius": 3
-                    }}
-                />
+                {markers_visible && <>
+
+
+                    <Layer
+                        id="flag-layer"
+                        type="symbol"
+                        layout={{ "icon-image": ["get", "project_type"], "icon-size": 0.6, "icon-anchor": "bottom-left",   "icon-allow-overlap": true, }}
+                        source="marker-source"
+                        filter={["==", ["get", "#"], highlight_id_or_str]}
+                    />
+                    <Layer
+                        id="markers-layer"
+                        type="circle"
+                        // type="symbol"
+                        // layout={{ "icon-image": "custom-marker", "icon-size": 0.05 }}
+                        source="marker-source"
+                        paint = {{ "circle-stroke-width": 0.5,
+                            "circle-color": [
+                                "match",
+                                ["get", "#"], highlight_id_or_str,
+                                "red",
+                                "transparent"
+                            ],
+                            "circle-stroke-color": "red",
+                            "circle-radius": 3
+                        }}
+                    />
+                </>
+                }
 
             </Source>
         </>
