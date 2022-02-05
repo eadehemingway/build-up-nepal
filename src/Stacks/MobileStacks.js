2e885458bfc70098df180ec52be195f773fcb107
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2022-02-05 23:03:00 +0100

    bar loading but rubbish

--- src/Stacks/MobileStacks.js
+++ src/Stacks/MobileStacks.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useMemo } from "react";
 import { useEffect } from "react/cjs/react.development";
 import styled from "styled-components";
 import { StackedBar } from "./StackedBar";
@@ -13,20 +13,23 @@ export function MobileStacks({ width, height, setHighlightLocked, highlight_id, 
     const [sort_by, setSortBy] = useState("year");
     const [window_width, setWindowWidth] = useState(document.body.clientWidth);
     const [stacked_data, setStackedData] = useState(null);
-    const [selected_metric, setSelectedMetric] = useState("jobs");
-
+    const [selected_metric, setSelectedMetric] = useState(null);
+    const options = [
+        { label: "Total jobs created", value: "Total jobs created" },
+        { label: "Total CO₂ saved", value: "Total CO₂ saved" },
+        { label: "Total houses built", value: "Total houses built" }
+    ];
     useEffect(() => {
         setWindowWidth(document.body.clientWidth);
         const stack_data = getStackedData(chart_height, chart_margin);
         setStackedData(stack_data);
+        setSelectedMetric(options[0].value);
     }, []);
 
-    const options = [
-        { label: "Jobs", value: "jobs" },
-        { label: "C02", value: "CO2" },
-        { label: "Houses", value: "houses" }
-    ];
-
+    const selected_stacked_data = useMemo(()=>{
+        const d = stacked_data?.find(d=> d.caption === selected_metric);
+        return d;
+    }, [selected_metric, stacked_data]);
     return (
         <StackedBarContainer width={width} height={height}>
             <Dropdowns>
@@ -38,28 +41,24 @@ export function MobileStacks({ width, height, setHighlightLocked, highlight_id, 
                     onSelect={setSelectedMetric}
                 />
             </Dropdowns>
+            {selected_stacked_data &&  <StackedBar
+                data={selected_stacked_data}
+                highlight_id={highlight_id}
+                setHighlightId={setHighlightId}
+                chart_margin={chart_margin}
+                chart_height={chart_height}
+                window_width={window_width}
+                width={width}
+                sort_by={sort_by}
+                setHighlightLocked={setHighlightLocked}
+                highlight_locked={highlight_locked}
+            />}
 
-            {/* {stacked_data && stacked_data.map((d,i)=> (
-                <StackedBar
-                    key={i}
-                    data={d}
-                    highlight_id={highlight_id}
-                    setHighlightId={setHighlightId}
-                    chart_margin={chart_margin}
-                    chart_height={chart_height}
-                    window_width={window_width}
-                    width={width}
-                    sort_by={sort_by}
-                    setHighlightLocked={setHighlightLocked}
-                    highlight_locked={highlight_locked}
-                />
-            ))} */}
         </StackedBarContainer>
     );
 }
 
 const StackedBarContainer = styled.div`
-
     height: ${({ height }) => height }px;
     position: absolute;
     bottom: 20px;
