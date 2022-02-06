58ba0c9e0cca21007ec7486cbcae5237977472c5
Eade Hemingway <eadehemingway@Eades-MacBook-Pro.local>
2022-02-06 22:19:22 +0100

    Add test image meta

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import MapGL, { Source, Layer, Marker } from "react-map-gl";
 import { Mobile } from "./AppMobile.js";
 import { Desktop } from "./AppDesktop.js";
 import { Helmet } from "react-helmet";
+import meta_img from "./assets/red-flag.png";
 
 var font = new FontFace("code-saver", "url(https://use.typekit.net/af/58ae6c/00000000000000007735b602/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3)");
 
@@ -31,6 +32,7 @@ function App() {
                     name="description"
                     content="Interactive map displaying the projects and enterprises created by Build up Nepal"
                 />
+                <meta property="og:image" content={meta_img} />
             </Helmet>
             {font_loaded &&
                (is_mobile ?
